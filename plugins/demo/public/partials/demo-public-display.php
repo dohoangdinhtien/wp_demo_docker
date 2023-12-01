e3948792980f87298026bbdcbd9bdcb4d2672c92
Tien Do <tiendo152@gmail.com>
2023-12-01 14:20:25 +0700

    add plugins

---
+++ plugins/demo/public/partials/demo-public-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a public-facing view for the plugin
+ *
+ * This file is used to markup the public-facing aspects of the plugin.
+ *
+ * @link       https://test.demo
+ * @since      1.0.0
+ *
+ * @package    Demo
+ * @subpackage Demo/public/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
